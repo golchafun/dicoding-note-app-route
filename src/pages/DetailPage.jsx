40ec10999d0bff50a9128aae9e9e23cd0264c03a
golchafun <wannabe.golcha@gmail.com>
2025-06-17 15:53:22 +0700

    menampilkan detail page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { getNote } from "../utils/local-data";
+import { showFormattedDate } from "../utils";
 
-function DetailPage(){
-    return(
-        <section>
-            <h2>Detail page here</h2>
+function DetailPage() {
+    const { id } = useParams();
+    const note = getNote(id);
+
+    if (!note) {
+        return (
+            <section>
+                <h2>404</h2>
+                <p>Page not found</p>
+            </section>
+        );
+    }
+    return (
+        <section className="detail-page">
+            <h3 className="detail-page__title">{note.title}</h3>
+            <p className="detail-page__createdAt">{showFormattedDate(note.createdAt)}</p>
+            <div className="detail-page__body">{note.body}</div>
+            <div className="detail-page__action">
+                <button className="action" type="button" title="Hapus"></button>
+            </div>
         </section>
     );
 }
