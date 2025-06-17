2427dc02a4a0626eb2590d500c7a5ba3fb84f697
golchafun <wannabe.golcha@gmail.com>
2025-06-17 14:30:18 +0700

    change how to show date

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { showFormattedDate } from "../utils";
 
 function NoteItem({ id, title, body, createdAt, archived }) {
     return (
         <article className="note-item">
             <h3 className="note-item__title">
                 <a href="/notes/notes-1">{title}</a>
-            </h3><p className="note-item__createdAt">{createdAt}</p>
+            </h3><p className="note-item__createdAt">{showFormattedDate(createdAt)}</p>
             <p className="note-item__body">{body}</p>
         </article>
     );
